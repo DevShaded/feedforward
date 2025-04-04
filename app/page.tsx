572315b8b422f6d515afe5f214410da0734d36c7
DevShaded <devshaded@devshaded.com>
2025-04-04 09:28:59 +0200

    feat(page): add metadata for the landing page with title and description

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import LandingPage from '@/components/LandingPage';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'FeedForward',
+  description: 'FeedForward hjelper deg med å samle inn, analysere og handle på tilbakemeldinger effektivt. Skap bedre produkter og tjenester gjennom meningsfulle brukerinnsikter.',
+}
 
 export default function Home() {
   return <LandingPage />;
